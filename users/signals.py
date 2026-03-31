c65afb3e27c4dec87a2bda0c4806dc2a490e8c4e
eiilorvabimrsu-cmd <eiilorvabimrsu@gmail.com>
2026-03-31 20:57:38 +0200

    Bootstrap production admin during migrate when env vars are set

--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,8 @@
+import os
+
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
 from .models import Profile
@@ -14,3 +17,23 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+@receiver(post_migrate)
+def ensure_bootstrap_admin(sender, **kwargs):
+    username = os.environ.get('BOOTSTRAP_ADMIN_USERNAME')
+    password = os.environ.get('BOOTSTRAP_ADMIN_PASSWORD')
+    email = os.environ.get('BOOTSTRAP_ADMIN_EMAIL', 'admin@example.com')
+
+    if not username or not password:
+        return
+
+    user, _ = User.objects.get_or_create(
+        username=username,
+        defaults={'email': email, 'is_staff': True, 'is_superuser': True},
+    )
+    user.email = user.email or email
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password(password)
+    user.save()
